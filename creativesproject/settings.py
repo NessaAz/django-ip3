cf03d2d3c9691d9e6d23c09f0162c28a234f7cf6
NessaAz <vanessa.azenwa@gmail.com>
2022-06-13 00:23:33 +0300

    add templates directory path to settings

--- creativesproject/settings.py
+++ creativesproject/settings.py
@@ -44,7 +44,7 @@ ROOT_URLCONF = 'creativesproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -106,6 +106,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
