2a403261690b8691f7aa7127cf89210b286db452
NessaAz <vanessa.azenwa@gmail.com>
2022-06-14 16:30:18 +0300

    register profile img as cloudinary field

--- creativesapp/models.py
+++ creativesapp/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
 
 class Profile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     id_user = models.IntegerField
     bio = models.TextField(blank=True)
-    profileimg = models.ImageField(upload_to = 'profile_images')
+    profileimg = CloudinaryField('image')
     
     def __str__(self):
         return self.user.username
