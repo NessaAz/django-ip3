75b2fcd4a8755070905b050b9cd0e0edf2042333
NessaAz <vanessa.azenwa@gmail.com>
2022-06-14 21:17:39 +0300

    add rating class,register in sadmin,make migration

--- creativesapp/models.py
+++ creativesapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Profile(models.Model):
@@ -12,3 +13,14 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
     
+    
+class Rating(models.Model):
+    image = CloudinaryField('image')
+    score = models.IntegerField(default=0,
+                                validators=[
+                                    MaxValueValidator(100),
+                                    MinValueValidator(0)
+                                            ]
+                                )
+    def __str__(self):
+        return str(self.pk)
