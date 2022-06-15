9a487a04271ba179f6304748a68807f8388b7035
NessaAz <vanessa.azenwa@gmail.com>
2022-06-15 17:07:08 +0300

    add logou functionality

--- creativesapp/urls.py
+++ creativesapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('signup/', views.signup, name = 'signup'),
     path('login/', views.login, name ='login'),
     path('feed/', views.feed, name='feed'),
+    path('feed/logout', views.logout, name='logout')
 ]
