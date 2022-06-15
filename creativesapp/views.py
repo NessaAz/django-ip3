57854e6da009bf0059c274bb179811db8bb2ca3b
NessaAz <vanessa.azenwa@gmail.com>
2022-06-15 17:06:34 +0300

    add login views

--- creativesapp/views.py
+++ creativesapp/views.py
@@ -1,16 +1,17 @@
 from pyexpat.errors import messages
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 from django.contrib import auth
 from django.http import HttpResponse
 from .models import Profile
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 
 def index(request):
     return render(request, 'index.html')
 
-def signup(request):
-    
+def signup(request):    
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
@@ -32,10 +33,35 @@ def signup(request):
             return redirect('signup')
     else:
         
-        return render(request, 'signup.html')
-
+        return render(request, 'feed.html')
+    
+        
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        # form = AuthenticationForm(data=request.POST)
+        # if form.is_valid():
+        #     user = form.get_user()
+        #     login(user)
+        #     return redirect('feed')
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = auth.authenticate(username=username, password=password)
+        
+        if user is not None:
+            auth.login(request, user)
+            return redirect('feed')
+        else:
+            messages.info(request, 'username non existent')
+            return redirect('signup')
+    else:
+        #form = AuthenticationForm()    
+        return render(request, 'login.html')
 
+@login_required(login_url='login')
 def feed(request):
     return render(request, 'feed.html')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('logout')
